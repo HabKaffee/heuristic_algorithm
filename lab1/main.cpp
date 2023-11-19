72c470beda6a4acee849acbf33d900e6ed17a592
HabKaffee <neknaum1@gmail.com>
2023-11-19 13:01:53 +0300

    Not it passes with mark 9

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -4,15 +4,15 @@
 int main() {
   std::array<std::string, 10> filenames = {
     "test_data/myciel3.col",
-    // "test_data/myciel7.col",
-    // "test_data/school1.col",
-    // "test_data/school1_nsh.col",
-    // "test_data/anna.col",
-    // "test_data/miles1000.col",
-    // "test_data/miles1500.col",
-    // "test_data/le450_5a.col",
-    // "test_data/le450_15b.col",
-    // "test_data/queen11_11.col"
+    "test_data/myciel7.col",
+    "test_data/school1.col",
+    "test_data/school1_nsh.col",
+    "test_data/anna.col",
+    "test_data/miles1000.col",
+    "test_data/miles1500.col",
+    "test_data/le450_5a.col",
+    "test_data/le450_15b.col",
+    "test_data/queen11_11.col"
   };
   for (auto filename : filenames) {
     if (filename.empty()) {
@@ -24,14 +24,14 @@ int main() {
     std::vector<int> colors(vertices.size(), 0);
     auto colorizationResult = colorizeGraph(colors, edges, vertices);
     std::cout << filename << " <========================================\n";
-    // std::cout << "========================= Greedy coloring =============================\n";
-    // std::cout << colorizationResult.first << " " << colorizationResult.second << std::endl;
+    std::cout << "========================= Greedy coloring =============================\n";
+    std::cout << colorizationResult.first << " " << colorizationResult.second << std::endl;
     // std::cout << "========================================================================\n";
     std::fill(colors.begin(), colors.end(), 0);
     colorizationResult = colorizeGraph(colors, edges, vertices, true);
-    utils::printAdjustList(edges, vertices);
+    // utils::printAdjustList(edges, vertices);
     std::cout << "======================= Randomized coloring ============================\n";
-    std::cout << filename << " " << colorizationResult.first << " " << colorizationResult.second << std::endl;
+    std::cout << colorizationResult.first << " " << colorizationResult.second << std::endl;
     std::cout << "========================================================================\n";
     
   }
